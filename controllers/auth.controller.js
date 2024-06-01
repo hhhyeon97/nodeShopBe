bd34694502b133bad9c771928e044272a9bb8fa3
minsol <kth083030@naver.com>
2024-06-01 16:06:57 +0900

    토큰로그인테스트

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,9 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const authController = {};
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 authController.loginWithEmail = async (req, res) => {
   try {
@@ -15,6 +18,21 @@ authController.loginWithEmail = async (req, res) => {
       }
     }
     throw new Error('이메일 혹은 비밀번호가 잘못되었습니다 !');
+  } catch (error) {
+    res.status(400).json({ status: 'fail', error: error.message });
+  }
+};
+
+authController.authenticate = async (req, res, next) => {
+  try {
+    const tokenString = req.headers.authorization;
+    if (!tokenString) throw new Error('Token not found');
+    const token = tokenString.replace('Bearer ', '');
+    jwt.verify(token, JWT_SECRET_KEY, (error, payload) => {
+      if (error) throw new Error('Invalid token');
+      req.userId = payload._id;
+    });
+    next();
   } catch (error) {
     res.status(400).json({ status: 'fail', error: error.message });
   }

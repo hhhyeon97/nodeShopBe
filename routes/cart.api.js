62123bb669fd87ef8082b99be665f3648f249dcc
minsol <kth083030@naver.com>
2024-06-06 21:52:47 +0900

    카트데이터보여주기

--- routes/cart.api.js
+++ routes/cart.api.js
@@ -4,5 +4,5 @@ const cartController = require('../controllers/cart.controller');
 const router = express.Router();
 
 router.post('/', authController.authenticate, cartController.addItemToCart);
-
+router.get('/', authController.authenticate, cartController.getCart);
 module.exports = router;

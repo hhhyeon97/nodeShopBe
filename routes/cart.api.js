bd6012be98d650d4d55e75874e470411014b662f
minsol <kth083030@naver.com>
2024-06-07 00:46:32 +0900

    카트관련테스트

--- routes/cart.api.js
+++ routes/cart.api.js
@@ -5,4 +5,11 @@ const router = express.Router();
 
 router.post('/', authController.authenticate, cartController.addItemToCart);
 router.get('/', authController.authenticate, cartController.getCart);
+router.delete(
+  '/:id',
+  authController.authenticate,
+  cartController.deleteCartItem,
+);
+router.put('/:id', authController.authenticate, cartController.editCartItem);
+router.get('/qty', authController.authenticate, cartController.getCartQty);
 module.exports = router;

d94c56ea015259a282c20cda026c447869c9cb13
minsol <kth083030@naver.com>
2024-06-12 04:19:00 +0900

    오더리스트보여주기

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -40,4 +40,53 @@ orderController.createOrder = async (req, res) => {
   }
 };
 
+orderController.getOrder = async (req, res) => {
+  try {
+    const { userId } = req;
+
+    // 인증되지 않은 요청 처리
+    if (!userId) {
+      return res
+        .status(401)
+        .json({ status: 'fail', message: '로그인 후 이용 가능합니다.' });
+    }
+
+    // 페이지 번호와 페이지 크기 추출 (기본값: 1페이지 당 5개 항목)
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 5;
+    const skip = (page - 1) * limit;
+
+    // db에서 유저 주문 목록 가져오기
+    const orders = await Order.find({ userId })
+      .skip(skip)
+      .limit(limit)
+      .populate({
+        path: 'items',
+        populate: {
+          path: 'productId',
+          model: 'Product',
+        },
+      });
+
+    // 전체 주문 수
+    const totalOrders = await Order.countDocuments({ userId });
+
+    // 응답 데이터 구성
+    const response = {
+      status: 'success',
+      data: orders,
+      totalOrders,
+      totalPages: Math.ceil(totalOrders / limit),
+      currentPage: page,
+    };
+    res.status(200).json({ status: 'success', response });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: '주문 내역을 가져오는 도중 오류가 발생했습니다.',
+      error: error.message,
+    });
+  }
+};
+
 module.exports = orderController;

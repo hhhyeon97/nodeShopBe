b7a7aece8d504a5869786e40050b86940cf47cb1
minsol <kth083030@naver.com>
2024-06-16 11:19:25 +0900

    카테고리테스트

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -40,24 +40,54 @@ productController.createProduct = async (req, res) => {
   }
 };
 
+// productController.getProducts = async (req, res) => {
+//   try {
+//     const { page, name } = req.query;
+//     const cond = name
+//       ? { name: { $regex: name, $options: 'i' }, isDeleted: false }
+//       : { isDeleted: false };
+//     let query = Product.find(cond);
+//     let response = { status: 'success' };
+//     if (page) {
+//       // 몽구스 함수 skip(), limit()
+//       // ex ) 10개의 상품이 있는데 한 페이지당 5개씩 보여주고 싶으면
+//       // limit은 5가 되고 skip은 2페이지로 가면 1페이지의 5개는 제외하고(스킵)
+//       // 2페이지의 5개만 보여줄거라는 ...!?
+//       query.skip((page - 1) * PAGE_SIZE).limit(PAGE_SIZE);
+//       // 최종 몇개의 페이지가 있는지 !
+//       // 데이터가 총 몇개 있는지
+//       const totalItemNum = await Product.find(cond).count();
+//       // 데이터 총 개수 / PAGE_SIZE
+//       const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+//       response.totalPageNum = totalPageNum;
+//     }
+
+//     const productList = await query.exec();
+//     response.data = productList;
+//     res.status(200).json(response);
+//     // res.status(200).json({ status: 'success', data: productList });
+//   } catch (error) {
+//     res.status(400).json({ status: 'fail', error: error.message });
+//   }
+// };
+
 productController.getProducts = async (req, res) => {
   try {
-    const { page, name } = req.query;
-    const cond = name
-      ? { name: { $regex: name, $options: 'i' }, isDeleted: false }
-      : { isDeleted: false };
+    const { page, name, category } = req.query; // category 파라미터 추가
+    const cond = { isDeleted: false }; // 기본 조건
+
+    if (name) {
+      cond.name = { $regex: name, $options: 'i' }; // 이름 필터링 추가
+    }
+    if (category) {
+      cond.category = category; // 카테고리 필터링 추가
+    }
+
     let query = Product.find(cond);
     let response = { status: 'success' };
     if (page) {
-      // 몽구스 함수 skip(), limit()
-      // ex ) 10개의 상품이 있는데 한 페이지당 5개씩 보여주고 싶으면
-      // limit은 5가 되고 skip은 2페이지로 가면 1페이지의 5개는 제외하고(스킵)
-      // 2페이지의 5개만 보여줄거라는 ...!?
       query.skip((page - 1) * PAGE_SIZE).limit(PAGE_SIZE);
-      // 최종 몇개의 페이지가 있는지 !
-      // 데이터가 총 몇개 있는지
       const totalItemNum = await Product.find(cond).count();
-      // 데이터 총 개수 / PAGE_SIZE
       const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
       response.totalPageNum = totalPageNum;
     }
@@ -65,7 +95,6 @@ productController.getProducts = async (req, res) => {
     const productList = await query.exec();
     response.data = productList;
     res.status(200).json(response);
-    // res.status(200).json({ status: 'success', data: productList });
   } catch (error) {
     res.status(400).json({ status: 'fail', error: error.message });
   }
